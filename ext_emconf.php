624442211dd0d099bbe7a199d4bf771542544a60
Georg Ringer <georg.ringer@gmail.com>
2022-12-15 08:59:35 +0100

    [TASK] Release v1.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mail@ringer.it',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'constraints' =>
         [
             'depends' => [
