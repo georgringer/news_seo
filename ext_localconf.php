173db0e1c2b9e32f37d510290df87e14fefa05d3
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-09-13 17:58:44 +0200

    Remove Slots

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,12 +2,3 @@
 
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = 'news_seo';
 
-if (!class_exists(\GeorgRinger\News\Event\NewsDetailActionEvent::class)) {
-    $signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher');
-    $signalSlotDispatcher->connect(
-        'GeorgRinger\\News\\Controller\\NewsController',
-        'detailAction',
-        'GeorgRinger\\NewsSeo\\Slots\\NewsControllerSlot',
-        'detailActionSlot'
-    );
-}
