e4e2154e2328cdb37dbfdb85e717058178d24abb
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-10-29 13:46:29 +0100

    [bugfix] Remove redundant condition, that prevents the hreflang generation for news-articles on pages with no_index=1; resolves #26

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'mail@ringer.it',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '2.2.0',
+    'version' => '2.2.1',
     'constraints' =>
         [
             'depends' => [
